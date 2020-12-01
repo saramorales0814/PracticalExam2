a296eb3ec59d432ae90a45c97aba26aad69e4653
saramorales0814 <72719912+saramorales0814@users.noreply.github.com>
2020-11-30 20:36:44 -0700

    Update q1.c

--- q1.c
+++ q1.c
@@ -11,15 +11,60 @@ int isHeterosquare(int size, int square[size][size]){
   int sums[size*2 + 2];
 
   // Sum horizontal
+  {
+    sum = 0;
+    {
+      sum = sum +square[i][j];
+    }
   
 
   // Sum Vertical
+    {
+      sum = 0;
+      {
+        sum + sum +square[j][i]
+      }
   
 
   // Sum Diagonals
+    sum = 0;
+      {
+        sum = sum +square[i][size-i-1];
+      }
+      sums[k] = sum;
+      k++;
   
 
   // Check if all the sums are different
+      {
+        find = sums[i];
+        {
+          return 0;
+        }
+      }
+    }
 
   return 1;
 }
+void main()
+{
+  int s;
+  printf("Enter size\n");
+  scanf("%d%,&s);
+        
+  int sqr[s][s];
+  
+  readSquare(s,sqr);
+  
+  if(isHeterosquares(s,sqr))
+   
+        {
+          printf("It is Heterosquare\n");
+        }
+        
+        else
+        {
+          print("It isnt Heterosquare");
+        }
+        }
+     
